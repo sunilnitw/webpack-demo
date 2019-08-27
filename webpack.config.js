a4ce9f10d029821879a9ee04e4969ec92273aa16
sunil32 <sunil32.kumar@paytmmall.com>
2019-08-27 20:08:03 +0530

    bootstrap install and loaded from sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"] //Order matters from end, css-loader, turn into javascript, 
+                test: /\.scss$/,
+                use: [
+                    "style-loader", //3. Inject style to the DOM
+                    "css-loader", //2. Turn CSS to Common Js
+                    "sass-loader" // 1. Turn SCSS to CSS
+                ] //Order matters from end, css-loader, turn into javascript, 
             }
         ]
     }
