d5856265377167f01c88a52f42bb06449d488350
sunil32 <sunil32.kumar@paytmmall.com>
2019-08-27 20:53:05 +0530

    html webpack plugin added and hashing the main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,9 +6,12 @@ module.exports = {
     //devtool: 'none', // remove the eval from generate code
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contentHash].js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
     module: {
         rules: [
             {
